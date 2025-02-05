873f8c1b6fce90832189b1bc84b46ad44c559c75
Sıla Bozkurt <silabzkrtt@hotmail.com>
2025-02-05 16:15:22 +0300

    fixed code

--- Cipher.java
+++ Cipher.java
@@ -7,6 +7,7 @@ public class Cipher
     public static final String ORIGINAL_ALPHABET = "abcdefghijklmnopqrstuvwxyz";
     public static final String CIPHER_ALPHABET = "dfxyhrklvwuasgimnojpqetbcz";
 
+
     public String encrypt(String inputString) {
         
         // output string will be collected in this variable, one char at a time
@@ -15,7 +16,7 @@ public class Cipher
         // for all chars in the input string
         for (int i = 0; i < inputString.length(); i++)   
         {
-
+            outputString += replaceChar(inputString.charAt(i), true);
         }
 
         return outputString;
@@ -25,8 +26,9 @@ public class Cipher
         
         // output string will be collected in this variable, one char at a time
         String outputString = "";
-        
-        replaceChar('a',true);
+        for (int i = 0; i < inputString.length(); i++) {
+            outputString +=  replaceChar(inputString.charAt(i),true);
+        }
         
         return outputString;
     }
@@ -42,7 +44,7 @@ public class Cipher
             for (int i = 0; i < ORIGINAL_ALPHABET.length(); i++)   
             {
                 if(ORIGINAL_ALPHABET.charAt(i) == inputChar) {
-
+                    return CIPHER_ALPHABET.charAt(i);
                 }
             }
         }
